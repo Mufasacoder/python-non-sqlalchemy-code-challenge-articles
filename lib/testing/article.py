7db6e18fb041d111b8e4037421073ab2d8b2282b
mufasacoder <edwardmwangi94@gmail.com>
2024-06-06 14:32:47 +0300

    initial commit

---
+++ lib/testing/article.py
@@ -0,0 +1,19 @@
+def __init__(self, model, year, make):
+        self.model = model
+        self.year = year
+        self.make = make
+    @classmethod
+def drive(self)
+     #pass 
+     return "I am driving"
+    
+def reverse(self)#methodificartion
+        return self.drive
+    
+def brake(self)
+       
+def turnLeft(cls)
+       return "I am turning left"
+
+def getObject(self)
+       return self
